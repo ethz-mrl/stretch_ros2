f51c1f58768eb68f452d6fc37c1f9a77a8e08983
Matteo <matteo.iovino93@gmail.com>
2026-04-14 13:32:04 +0200

    Add use_sim_time param to joint_state_publisher as well

--- stretch_simulation/launch/stretch_mujoco_driver.launch.py
+++ stretch_simulation/launch/stretch_mujoco_driver.launch.py
@@ -59,6 +59,7 @@ def generate_launch_description():
                 {"source_list": ["/stretch/joint_states"]},
                 {"rate": 30.0},
                 {"robot_description": robot_description_content},
+                {"use_sim_time": True},
             ],
             arguments=["--ros-args", "--log-level", "error"],
         )
